57bffc785c52e2374bc73ebc46adaf2a239adf8f
Tom Morton <tomm@riseup.net>
2018-07-22 22:00:59 +0100

    Transfer listing spawner

--- src/Fitba/TransferMarket.hs
+++ src/Fitba/TransferMarket.hs
@@ -1,23 +1,37 @@
 module Fitba.TransferMarket where
 import System.Random (RandomGen, newStdGen)
+import Control.Monad.IO.Class (liftIO)
 import qualified Control.Monad.Random as Random
 import qualified Data.Text as T
+import qualified Database.Persist as P
+import qualified Data.Time.Clock
 
 import qualified Fitba.Player
 import Fitba.Schema
+import Fitba.DB (MonadDB, Con)
+import qualified Fitba.Types as Types
 
 decideTransferMarketBids :: IO ()
 decideTransferMarketBids = pure ()
 
-spawnNewTransferListings :: [T.Text] -> IO ()
+spawnNewTransferListings :: MonadDB a => [T.Text] -> Con a ()
 spawnNewTransferListings surnamePool = do
-  g <- System.Random.newStdGen
+  g <- liftIO System.Random.newStdGen
   let maybePlayer = fst $ Random.runRand (maybeRandomPlayer surnamePool) g
   case maybePlayer of
     Nothing -> pure ()
     Just player -> do
-      putStrLn "Would make player:"
-      print player
+      now <- liftIO Data.Time.Clock.getCurrentTime
+      key <- P.insert player
+      P.insert $ TransferListing
+        key
+        (200000 * playerSkill player)
+        (Data.Time.Clock.addUTCTime (60*60*24) now)
+        Nothing
+        Nothing
+        Types.Active
+
+      liftIO $ putStrLn $ "Transfer listing created: " ++ show player
 
 maybeRandomPlayer :: (RandomGen g) => [T.Text] -> (Random.Rand g) (Maybe Player)
 maybeRandomPlayer surnamePool = do
--- src/daemon/Main.hs
+++ src/daemon/Main.hs
@@ -1,31 +1,40 @@
 {-# LANGUAGE OverloadedStrings     #-}
 module Main where
 
+import Control.Monad.Logger (runStderrLoggingT, MonadLogger)
 import Data.IORef
 import Data.Time.Clock
 import Data.Time.Calendar
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Control.Monad.IO.Class (liftIO)
 import Control.Monad
+import Database.Persist.Sqlite (runSqlite)
 import Control.Concurrent (threadDelay)
 import System.Exit (exitSuccess)
 
 import qualified Fitba.TransferMarket
+import qualified Fitba.DB as DB
 
-data DaemonData = DaemonData { surnamePool :: [T.Text] }
+data DaemonData = DaemonData {
+  surnamePool :: [T.Text],
+  lastTick :: IORef (Maybe UTCTime)
+  }
+
+runDB = runSqlite "live.db"
 
 simulateSeconds :: Integer -> IO ()
 simulateSeconds seconds =
-    replicateM_ (fromInteger seconds) $ do
-      -- simulate games
-      putStr "."
+  replicateM_ (fromInteger seconds) $ do
+    -- simulate games
+    putStr "."
 
 oncePerMinute :: DaemonData -> IO ()
 oncePerMinute daemonData = do
   t <- getCurrentTime
   putStrLn $ "New minute! " ++ show t
   Fitba.TransferMarket.decideTransferMarketBids
-  Fitba.TransferMarket.spawnNewTransferListings (surnamePool daemonData)
+  runDB $ Fitba.TransferMarket.spawnNewTransferListings (surnamePool daemonData)
 
 oncePerDay :: UTCTime -> IO ()
 oncePerDay t = do
@@ -34,39 +43,37 @@ oncePerDay t = do
     -- update team formations
     print t
 
-callback :: IORef (Maybe UTCTime) -> DaemonData -> IO ()
-callback ref daemonData = do
-    timestamp <- getCurrentTime
-    lastRun <- readIORef ref
-    case lastRun of
-        Nothing -> do
-            putStrLn "First run!"
-        Just t -> do
-            Fitba.TransferMarket.spawnNewTransferListings (surnamePool daemonData)
-            --putStrLn $ "Last run at " ++ show t
-            simulateSeconds (secondsDiff timestamp t)
-            if minutesDiff timestamp t > 0 then oncePerMinute daemonData else pure ()
-            when (dayChanged t timestamp) $ oncePerDay timestamp
+callback :: DaemonData -> IO ()
+callback daemonData = do
+  timestamp <- getCurrentTime
+  lastRun <- readIORef (lastTick daemonData)
+  case lastRun of
+    Nothing ->
+      putStrLn "First run!"
+    Just t -> do
+      --putStrLn $ "Last run at " ++ show t
+      simulateSeconds (secondsDiff timestamp t)
+      if minutesDiff timestamp t > 0 then oncePerMinute daemonData else pure ()
+      when (dayChanged t timestamp) $ oncePerDay timestamp
 
-    writeIORef ref $ Just timestamp
+  writeIORef (lastTick daemonData) $ Just timestamp
 
-    where
-        secondsDiff :: UTCTime -> UTCTime -> Integer
-        secondsDiff a b =
-            ((diffTimeToPicoseconds . utctDayTime) a `quot` 1000000000000) -
-            ((diffTimeToPicoseconds . utctDayTime) b `quot` 1000000000000)
+  where
+    secondsDiff :: UTCTime -> UTCTime -> Integer
+    secondsDiff a b =
+      ((diffTimeToPicoseconds . utctDayTime) a `quot` 1000000000000) -
+      ((diffTimeToPicoseconds . utctDayTime) b `quot` 1000000000000)
 
-        minutesDiff :: UTCTime -> UTCTime -> Integer
-        minutesDiff a b =
-            ((diffTimeToPicoseconds . utctDayTime) a `quot` 60000000000000) -
-            ((diffTimeToPicoseconds . utctDayTime) b `quot` 60000000000000)
+    minutesDiff :: UTCTime -> UTCTime -> Integer
+    minutesDiff a b =
+      ((diffTimeToPicoseconds . utctDayTime) a `quot` 60000000000000) -
+      ((diffTimeToPicoseconds . utctDayTime) b `quot` 60000000000000)
 
-        dayChanged :: UTCTime -> UTCTime -> Bool
-        dayChanged a b = utctDay a /= utctDay b
+    dayChanged :: UTCTime -> UTCTime -> Bool
+    dayChanged a b = utctDay a /= utctDay b
 
 main :: IO ()
 main = do
   surnames <- T.splitOn "\n" <$> TIO.readFile "surname.txt"
-  print (take 5 surnames)
   newIORef Nothing >>= \ref ->
-      forever $ threadDelay 250000 >> callback ref (DaemonData surnames)
+    forever $ threadDelay 250000 >> callback (DaemonData surnames ref)
