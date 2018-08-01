b29e49a15f242cde9fcbf6110f778337f038d12c
Tom Morton <tomm@riseup.net>
2018-08-01 17:46:23 +0100

    Don't log debug messages

--- src/Fitba/DB.hs
+++ src/Fitba/DB.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE ConstraintKinds            #-}
 module Fitba.DB where
-import Control.Monad.Logger (NoLoggingT, runStderrLoggingT, MonadLogger)
+import Control.Monad.Logger (NoLoggingT, runStderrLoggingT, filterLogger, LogLevel(..), MonadLogger)
 import Control.Monad.Trans.Reader (ReaderT)
 import Control.Monad.Trans.Resource (ResourceT, MonadBaseControl)
 import Control.Monad.IO.Class (MonadIO)
@@ -15,7 +15,10 @@ type MonadDB a = (MonadIO a, MonadCatch a, MonadLogger a)  -- needs ConstraintKi
 type Con a b = ReaderT SqlBackend a b
 type ConnectionPool = Database.Persist.Postgresql.ConnectionPool
 
-getPool connString maxConns f = runStderrLoggingT $ withPostgresqlPool connString maxConns f
+getPool connString maxConns f =
+  runStderrLoggingT
+    $ filterLogger (\_ level -> level /= LevelDebug)
+    $ withPostgresqlPool connString maxConns f
 
 getFixtures :: MonadDB a => Con a [Entity Schema.Game]
 getFixtures = do
